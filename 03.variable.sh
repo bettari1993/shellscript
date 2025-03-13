94d61c578044fb4c0444c831ae8c97f47749d4a5
bettari1993 <rajubettari1993@gmail.com>
2025-03-13 10:33:16 +0530

    created variable.sh first file

---
+++ 03.variable.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PERSION1 = Raju
+PERSION2 = Nagesh
+
+echo "$PERSION1::${PERSION2},How are you ?"
+echo "$PERSION2::${PERSION1},i am fine,How are doing?"
+echo "$PERSION1::${PERSION2}.i am doing good,what's going on?"
+echo "$PERSION2::${PERSION1}.i Started learning shell script"
