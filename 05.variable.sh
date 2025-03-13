f04d8dc618c858f984fdbcb7b8a32dfbb22c90f5
bettari1993 <rajubettari1993@gmail.com>
2025-03-13 11:00:25 +0530

    variable 5 created

---
+++ 05.variable.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "please enter the username::"
+read -s USERNAME
+echo "Username entered is: $USERNAME"
+echo "Please enter your password::"
+read -s PASSWORD
+echo "Password entered is"$PASSWORD"
