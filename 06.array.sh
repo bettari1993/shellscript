34e14660e18c98fbb93d706bb0491c443e290403
bettari1993 <rajubettari1993@gmail.com>
2025-03-13 11:19:00 +0530

    created array.sh file

--- 06.array.sh
+++ 06.array.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-#index starts from 0, size is 3
-FRUITS=("APPLE" "GRAPES" "ORANGE") #Array
+# index starts from 0, size is 3
+FRUITS=("APPLE" "KIWI" "ORANGE") #Array
 
 echo "First fruit is: ${FRUITS[0]}"
 echo "Second fruit is: ${FRUITS[1]}"
 echo "Third fruit is: ${FRUITS[2]}"
 
 echo "First fruit is: ${FRUITS[@]}"
+
