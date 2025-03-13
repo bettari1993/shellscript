e45189b2f2fc73a31e48155b3a3b1ae31d05ca60
bettari1993 <rajubettari1993@gmail.com>
2025-03-13 10:22:10 +0530

    ranamed hello-world name added new file conversation.sh

---
+++ 02.conversation.sh
@@ -0,0 +1,5 @@
+#!/bin/basht
+echo "Ramesh:: Hi Raju how are you"
+echo "Raju:: Hello Ramesh,I am fine how are doing"
+echo "Ramesh:: I am doing good raju,What's going on"
+echo "Raju:: I started learning shell scripting"
