0331073a26c782446af7cba1d5ac159a02b66b0f
bettari1993 <rajubettari1993@gmail.com>
2025-03-13 10:47:56 +0530

    new file created as a variable 4

---
+++ 04.variable.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PERSION1=$1
+PERSION2=$2
+
+echo "$PERSION1::${PERSION2},How are you ?"
+echo "${PERSION2}::Hello $PERSION1.i am fine. How are you doing?"
+echo "$PERSION1::i am doing good ${PERSION2}. what's going on?"
+echo "${PERSION2}::i Started learning shell script $PERSION1"
