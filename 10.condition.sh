1c7fdc1b7fb212975f4bdf1efa24cb8d0f1b3bf1
bettari1993 <rajubettari1993@gmail.com>
2025-03-19 22:56:27 +0530

    added condition file

--- 10.condition.sh
+++ 10.condition.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-
 NUMBER=$1
 
-if [ $NUMBER -gt 20 ] #gt, lt, eq, -ne, -ge, -le
-then
+if [ "$NUMBER" -gt 20 ]; then
     echo "Given number: $NUMBER is greater than 20"
 else
-    echo "Given number: $NUMBER is less than 20"
-fi     
+    echo "Given number: $NUMBER is less than or equal to 20"
+fi
